b942942ae359d52c490eedb6734c0d24e4590a48
Boyang13 <112663743+Boyang13@users.noreply.github.com>
2023-01-11 10:49:27 -0800

    Update file.c

--- file.c
+++ file.c
@@ -2,24 +2,49 @@
 int main() {
         FILE * fp;
         char c;
+        char buff[255];  // create a buffer for reading the file
         printf("File Handling\n");
-        fp = fopen("demo.txt", "w");
-        while ((c = getchar()) != ' ') {
-            putc(c, fp);
+        //open a file
+        fp = fopen("demo.txt", "w");   //this is to open the file
+        //writing operation
+        while ((c = getchar()) != '\t') {
+            fputc(c, fp);
         }
+        //close fileclear
         fclose(fp);
         printf("Data Entered:\n");
-        fp = fopen("demo.txt", "r"); //prints what user entered
-        printf("1. ");
-        int i = 2; //starts at 2 because already prints 1
+        //reading
+        fp = fopen("demo.txt", "r");
         while ((c = getc(fp)) != EOF) {
             printf("%c", c);
-            if (c=='\n'){
-                printf("%d. ",i);
-                i++;
-            }
         }
-        fclose(fp); //closes and prints
+        fclose(fp);
+         printf("\n");
+          printf("Adding to the file\n");
+        fp = fopen("demo.txt", "a");   //this is to open the file
+        //writing operation
+        while ((c = getchar()) != '\t') {
+            fputc(c, fp);
+        }
+        //close file
+        fclose(fp);
+        printf("Data Entered after append:");
+        //reading
+        fp = fopen("demo.txt", "r");
+        while ((c = getc(fp)) != EOF) {
+            printf("%c", c);
+        }
+        fclose(fp);
         printf("\n");
+        fp = fopen("demo.txt", "r");
+        int i = 1;
+        while ((feof(fp) != 1))
+        // section to read one line of the file
+        {
+            fscanf(fp, "%s", buff);
+            printf("%d: %s\n",i, buff );
+            i++;
+        }
+        fclose(fp);
         return 0;
     }
